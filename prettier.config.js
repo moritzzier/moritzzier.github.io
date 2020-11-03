7a24dcc1dd55c106e7419f5e339dcb07700fd503
Moritz Zier <moritz.zier@web.de>
2020-11-03 17:51:40 +0100

    added stuff

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	printWidth: 100,
+	useTabs: true,
+	trailingComma: 'es5',
+	tabWidth: 4,
+	semi: true,
+	singleQuote: true,
+	jsxBracketSameLine: true,
+	arrowParens: 'always',
+	htmlWhitespaceSensitivity: 'strict',
+};
