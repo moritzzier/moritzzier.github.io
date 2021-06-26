3827944bf376d60ea133c2161cd419f6a3ea0ac5
moritzzier <moritz.zier@web.de>
2021-06-26 13:46:15 +0000

    Deploying to gh-pages from @ moritzzier/moritzzier.github.io@ec6e4c8d649d9d5858dd6b2e767f2b0a71daef34 🚀

---
+++ _next/static/6zJ1_hkyUQYFGw74zc6X9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f1dd48f3d15352c83b9f.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
